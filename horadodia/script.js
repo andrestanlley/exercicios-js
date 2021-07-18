947ef7fde804ec1db4af120bafb0aa26ac43738d
André Stanlley <stanlley41@gmail.com>
2021-07-18 00:46:04 -0300

    Title dinâmico

--- horadodia/script.js
+++ horadodia/script.js
@@ -4,16 +4,20 @@ var mostrarhora = document.getElementById('hora')
 var mostrarperiodo = document.getElementById('periodo')
 var mostrarimagem = document.getElementById('img')
 mostrarhora.innerText = `${hora.getHours()}:${hora.getMinutes()}H`
-if (agora < 12) {
-    mostrarperiodo.innerText = 'manhã.'
-    mostrarimagem.src = 'img/manha.jpg'
-    document.body.style.background = '#c46d02'
-} else if (agora >= 12 && agora < 18) {
-    mostrarperiodo.innerText = 'tarde.'
-    mostrarimagem.src = 'img/tarde.jpg'
-    document.body.style.background = '#fec59e'
-} else {
-    mostrarperiodo.innerText = 'noite.'
-    mostrarimagem.src = 'img/noite.jpg'
-    document.body.style.background = '#12263e'
+
+function alterar(){
+    if (agora < 12) {
+        mostrarperiodo.innerText = 'manhã.'
+        mostrarimagem.src = 'img/manha.jpg'
+        document.body.style.background = '#c46d02'
+        document.title = `Hora atual: ${hora.getHours()}:${hora.getMinutes()}H`
+    } else if (agora >= 12 && agora < 18) {
+        mostrarperiodo.innerText = 'tarde.'
+        mostrarimagem.src = 'img/tarde.jpg'
+        document.body.style.background = '#fec59e'
+    } else {
+        mostrarperiodo.innerText = 'noite.'
+        mostrarimagem.src = 'img/noite.jpg'
+        document.body.style.background = '#12263e'
+    }
 }
