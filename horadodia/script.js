326b5fd5c83bd4feaf99c254865cadb070f1ff07
André Stanlley <stanlley41@gmail.com>
2021-07-18 00:25:42 -0300

    Initial commit

---
+++ horadodia/script.js
@@ -0,0 +1,19 @@
+var hora = new Date()
+var agora = hora.getHours()
+var mostrarhora = document.getElementById('hora')
+var mostrarperiodo = document.getElementById('periodo')
+var mostrarimagem = document.getElementById('img')
+mostrarhora.innerText = `${hora.getHours()}:${hora.getMinutes()}H`
+if (agora < 12) {
+    mostrarperiodo.innerText = 'manhã.'
+    mostrarimagem.src = 'img/manha.jpg'
+    document.body.style.background = '#c46d02'
+} else if (agora >= 12 && agora < 18) {
+    mostrarperiodo.innerText = 'tarde.'
+    mostrarimagem.src = 'img/tarde.jpg'
+    document.body.style.background = '#fec59e'
+} else {
+    mostrarperiodo.innerText = 'noite.'
+    mostrarimagem.src = 'img/noite.jpg'
+    document.body.style.background = '#12263e'
+}
