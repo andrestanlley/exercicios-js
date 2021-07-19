cb525d4ce66f0eba1947638f99ee5c46e9426b58
André Stanlley <stanlley41@gmail.com>
2021-07-18 22:12:20 -0300

    mudanças nos textos

--- horadodia/script.js
+++ horadodia/script.js
@@ -3,20 +3,20 @@ var agora = hora.getHours()
 var mostrarhora = document.getElementById('hora')
 var mostrarperiodo = document.getElementById('periodo')
 var mostrarimagem = document.getElementById('img')
-mostrarhora.innerText = `${hora.getHours()}:${hora.getMinutes()}H`
+mostrarhora.innerText = `${hora.getHours()}:${hora.getMinutes()}h.`
 
 function alterar(){
+    document.title = `Hora atual: ${hora.getHours()}:${hora.getMinutes()}h.`
     if (agora < 12) {
-        mostrarperiodo.innerText = 'manhã.'
+        mostrarperiodo.innerText = 'Bom dia!'
         mostrarimagem.src = 'img/manha.jpg'
         document.body.style.background = '#c46d02'
-        document.title = `Hora atual: ${hora.getHours()}:${hora.getMinutes()}H`
     } else if (agora >= 12 && agora < 18) {
-        mostrarperiodo.innerText = 'tarde.'
+        mostrarperiodo.innerText = 'Boa tarde!'
         mostrarimagem.src = 'img/tarde.jpg'
         document.body.style.background = '#fec59e'
     } else {
-        mostrarperiodo.innerText = 'noite.'
+        mostrarperiodo.innerText = 'Boa noite!'
         mostrarimagem.src = 'img/noite.jpg'
         document.body.style.background = '#12263e'
     }
