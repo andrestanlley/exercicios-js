da758f9c7198bf130655199eedb6af5b59931278
André Stanlley <stanlley41@gmail.com>
2021-07-29 19:35:26 -0300

    Delete start.js

--- horadodia/array/start.js
+++
@@ -1,8 +0,0 @@
-let palavra = ''
-let fatia = palavra.split('')
-let inverso = palavra.split('').reverse()
-if (fatia.join() == inverso.join()){
-    console.log(`${palavra} É um palindromo!`)
-} else {
-    console.log(`${palavra} NÃO é um palindromo!`)
-}
